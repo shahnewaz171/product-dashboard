eed73781c1547390dce208c3fadb796abda11c4b
shahnewaz171 <shahnewaz601@gmail.com>
2022-06-02 12:17:00 +0600

    added file-loader for import image issue

--- src/components/shared/NotFound/NotFound.tsx
+++ src/components/shared/NotFound/NotFound.tsx
@@ -2,13 +2,14 @@ import React from 'react';
 import { Box, Typography } from '@mui/material';
 import { Main } from '../CustomStyles/CustomStyles';
 import localImages from '../localImages/localImages';
+import logoImg from '../../../images/404.png';
 
 const NotFound: React.FC = () => {
 
     return (
         <Main>
             <Box sx={{ height: "60vh", display: "flex",  justifyContent: "center"  }}>
-                <Typography component='img' src={localImages.logoImg} sx={{ height: 1 }} />
+                <Typography component='img' src={logoImg} sx={{ height: 1 }} />
             </Box>
             <Typography component='h5' sx={{ fontWeight: "bold", textAlign: "center", my: 2 }}>
                 Page not found!
